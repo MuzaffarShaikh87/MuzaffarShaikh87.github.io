209c01b5afc5db8c48a7bc07b98834d14092981a
Muzaffar <muzaffarshaikh517@gmail.com>
2020-11-12 16:57:00 -0700

    Updated Text in Slide

--- index.js
+++ index.js
@@ -13,9 +13,16 @@ var myPresentation = new Presenta('#app',
             textVar: 'title',
             step: 'p,.step',
             text: `<h3><mark>Use Arrows to move forward</mark></h3>
-              <h1><span class="step">This</span> 
-              <span class="step">Title</span> 
-              <span class="step">Steps</span></h1>
+              <h1><span class="step">Hi There</span> 
+              <span class="step">Welcome</span> 
+              <span class="step">to</span> 
+              <span class="step">Presenta</span> 
+              <span class="step">This</span> 
+              <span class="step">is</span> 
+              <span class="step">Slide</span> 
+              <span class="step">Layout</span> 
+              <span class="step">to</span>
+              <span class="step">Demonstrate</span></h1>
               <p>Also</p>
               <p>These</p>
               <p>Paragraphs</p>`
